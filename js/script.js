6ea1a2941dcff1283da9a9b1041dbda1d03b790c
Chi-Luc <chilucdiep@hotmail.com>
2021-01-13 20:30:06 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,98 @@
+const X_CLASS = 'x'
+const O_CLASS = 'o'
+const WINNING_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2],
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const winningMessageTextElement = document.querySelector(".winning-message-text")
+const replayButton = document.getElementById('replayButton')
+let oTurn
+
+startGame()
+
+// Replay Game
+replayButton.addEventListener('click', startGame)
+
+function startGame() {
+    winningMessageElement.classList.remove('show')
+    oTurn = false;
+    cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(O_CLASS)
+        cell.removeEventListener('click', handleClick)
+        cell.addEventListener('click', handleClick, { once: true })
+    })
+    setBoardHoverClass()
+}
+
+//Check turn and place a mark
+
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = oTurn ? O_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    if (checkWin(currentClass)) {
+        endGame(false)
+    } else if (isDraw()){
+        endGame(true)
+    } else {
+        swapTurns()
+        setBoardHoverClass()
+    }
+}
+
+function endGame(draw) {
+    if (draw) {
+        winningMessageTextElement.innerText = 'Draw!'
+    } else {
+        winningMessageTextElement.innerText = `${oTurn ? "O's": "X's"} Wins!`
+    }
+    winningMessageElement.classList.add('show')
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+// Switch turns
+function swapTurns() {
+    oTurn = !oTurn
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(O_CLASS)
+    board.classList.remove(X_CLASS)
+    if (oTurn) {
+        board.classList.add(O_CLASS)
+    } else {
+        board.classList.add(X_CLASS)
+    }
+}
+
+// Check for win
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
+}
+// Check for draw
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) ||
+        cell.classList.contains(O_CLASS)
+    })
+}
+
+// Replay Game
+replayButton.addEventListener('click', startGame)
